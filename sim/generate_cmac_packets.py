6a225f5ee2ddb6e0df622981320a7dd09dc1c585
twodoorcoupe <giorgio.fontanive@outlook.com>
2025-09-10 13:01:31 +0200

    Fix memory controller issues

--- sim/generate_cmac_packets.py
+++ sim/generate_cmac_packets.py
@@ -6,15 +6,20 @@ dst_mac = "55:44:33:22:11:00"
 src_ip = "1.2.3.4"
 dst_ip = "4.3.2.1"
 
-PAYLOAD_SIZE = 1024 - 1 - 8
+PAYLOAD_SIZE = 1024 - 4 - 8
 payload = bytes.fromhex("deadbeef") * (PAYLOAD_SIZE // 4)
-payload = payload[:PAYLOAD_SIZE]
+# payload = payload[:PAYLOAD_SIZE]
 
 opcode = 1  # Write
 key = 25
-packet = axi_packets.make_replication_packet(dst_mac=dst_mac, src_mac=src_mac, dst_ip=dst_ip, src_ip=src_ip, opcode=opcode, key=key, payload=payload)
+write_packet = axi_packets.make_replication_packet(dst_mac=dst_mac, src_mac=src_mac, dst_ip=dst_ip, src_ip=src_ip, opcode=opcode, key=key, payload=payload)
+
+opcode = 0  # Read
+read_packet = axi_packets.make_replication_packet(dst_mac=dst_mac, src_mac=src_mac, dst_ip=dst_ip, src_ip=src_ip, opcode=opcode, key=key, payload="")
 
 axi_packets.interface_manager.open_interfaces()
-axi_packets.send_packets("cmac0", packet)
+axi_packets.send_packets("cmac0", write_packet)
+axi_packets.make_cycles_delay("cmac0", 1000)
+axi_packets.send_packets("cmac0", read_packet)
 axi_packets.interface_manager.close_interfaces()
 
